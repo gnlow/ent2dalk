29b9c65f9f9633e94de8c2534b666d9f244d1cee
Gnlow <entrydark@gmail.com>
2020-03-05 16:30:53 +0900

    success

--- index.ts
+++ index.ts
@@ -4,26 +4,33 @@ import {
     BlockGroup,
     Thing,
     Project,
+    Vector,
+    Type,
 } from "dalkak";
 
 import * as entry from "@dalkak/entry";
 
 let project = new Project;
 project.mount(...Object.entries(entry).map(a => a[1]));
-var toDalkBlockGroup: (entBlockGroup: Array<any>) => BlockGroup = (entBlockGroup) => {
-    let returnValue = new BlockGroup({blocks: entBlockGroup.splice(1).map(toDalkBlock)});
-    project.pack.events.value[entBlockGroup[0].type].link(returnValue);
-    return returnValue;
+var toDalkBlockGroup: (entBlockGroup: Array<any>, _target: Thing) => BlockGroup = (entBlockGroup, _target) => {
+    if(project.pack.events.value[entBlockGroup[0].type]){
+        let returnValue = new BlockGroup({blocks: entBlockGroup.splice(1).map(a => toDalkBlock(a, _target))});
+        project.pack.events.value[entBlockGroup[0].type].link(returnValue);
+        return returnValue;
+    }else{
+        return new BlockGroup({blocks: entBlockGroup.map(a => toDalkBlock(a, _target))});
+    }
 };
 
-var toDalkBlock = (entBlock) => {
+var toDalkBlock = (entBlock, _target: Thing) => {
     let dalkBlock = project.pack.blocks.value[entBlock.type];
     let i = 0;
     let paramNames = [...Object.entries(dalkBlock.params.value).map(a => a[0])]; // 순서를 보장할 수 없음. 수정 필요.
     entBlock.params.forEach(entParam => {
         if(entParam){
+            console.log(entBlock.type, paramNames, i, paramNames[i])
             if(typeof entParam == "object"){
-                dalkBlock.setParam(paramNames[i], toDalkBlock(entParam));
+                dalkBlock.setParam(paramNames[i], toDalkBlock(entParam, _target));
             }else{
                 dalkBlock.setParam(paramNames[i], Literal.from(entParam));
             }
@@ -31,14 +38,32 @@ var toDalkBlock = (entBlock) => {
         }
     });
     entBlock.statements.forEach(entBlockGroup => {
-        dalkBlock.setParam(String(i), toDalkBlockGroup(entBlockGroup));
+        if(entBlockGroup){
+            dalkBlock.setParam(paramNames[i], toDalkBlockGroup(entBlockGroup, _target));
+        }
+        i++;
     });
-    
+    dalkBlock.setParam("_target", new Block({func: () => _target}));
+    dalkBlock.paramTypes.value._target = Type.fromConstructor(Thing);
     return dalkBlock;
 };
-export default Entry => 
+let convert = Entry => 
 Entry.container.objects_.forEach(entryObject => {
-    project.addThing(new Thing({
-        blockGroups: (entryObject.script.toJSON() as Array<any>).map(toDalkBlockGroup)
-    }));
+    let _target = new Thing;
+    _target.blockGroups = (entryObject.script.toJSON() as Array<any>).map(a => toDalkBlockGroup(a, _target));
+    project.addThing(_target);
 });
+export default convert;
+
+import test from "./test";
+
+(async () => {
+    var _target = new Thing({pos: new Vector(0, 0)});
+    _target.blockGroups = test.map(a => toDalkBlockGroup(a, _target))
+    project.addThing(_target);
+    _target.run();
+    //setTimeout(()=>console.log(project.thingGroup.children[0].pos),1000)
+    
+    //console.log(await (_target.blockGroups[0].blocks[0].params.value.code as BlockGroup).blocks[0].params)
+})()
+//console.log(project.thingGroup.children[0].blockGroups[0].blocks[0].params.value)
---
+++ test.ts
@@ -0,0 +1,87 @@
+export default [
+  [
+    {
+      "id": "gldy",
+      "x": 16.03997802734375,
+      "y": -59,
+      "type": "when_run_button_click",
+      "params": [
+        null
+      ],
+      "statements": [],
+      "movable": null,
+      "deletable": 1,
+      "emphasized": false,
+      "readOnly": null,
+      "copyable": true,
+      "extensions": []
+    },
+    {
+      "id": "5qva",
+      "x": 0,
+      "y": 0,
+      "type": "repeat_basic",
+      "params": [
+        {
+          "id": "43n4",
+          "x": 0,
+          "y": 0,
+          "type": "number",
+          "params": [
+            10
+          ],
+          "statements": [],
+          "movable": null,
+          "deletable": 1,
+          "emphasized": false,
+          "readOnly": null,
+          "copyable": true,
+          "extensions": []
+        },
+        null
+      ],
+      "statements": [
+        [
+          {
+            "id": "cn1t",
+            "x": 0,
+            "y": 0,
+            "type": "move_x",
+            "params": [
+              {
+                "id": "ci2j",
+                "x": 0,
+                "y": 0,
+                "type": "number",
+                "params": [
+                  "10"
+                ],
+                "statements": [],
+                "movable": null,
+                "deletable": 1,
+                "emphasized": false,
+                "readOnly": null,
+                "copyable": true,
+                "extensions": []
+              },
+              null
+            ],
+            "statements": [],
+            "movable": null,
+            "deletable": 1,
+            "emphasized": false,
+            "readOnly": null,
+            "copyable": true,
+            "extensions": []
+          }
+        ]
+      ],
+      "movable": null,
+      "deletable": 1,
+      "emphasized": false,
+      "readOnly": null,
+      "copyable": true,
+      "extensions": []
+    }
+  ]
+]
