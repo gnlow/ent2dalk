0e52d9773e0bf1cdfabe5b93a83c8207b732457a
Gnlow <entrydark@gmail.com>
2020-03-23 12:21:32 +0900

    지역변수 성공

--- realtest.ts
+++ realtest.ts
@@ -1,7 +1,7 @@
 import toDalk from "./index";
 
 import test from "./test"
-import { Block, BlockGroup, Literal } from "dalkak";
+import { Block, BlockGroup, Literal, Variable, Type, Template, Local } from "dalkak";
 
 import basic from "@dalkak/basic";
 
@@ -11,7 +11,7 @@ const log = new Block({
     name: "log",
     template: "{(val)을 출력하기}",
     func: ({val}, project, local) => {
-        console.log(val);
+        console.log("val", val);
     }
 });
 
@@ -23,11 +23,14 @@ const get = new Block({
         return variable.value ?? (variable.value = 0);
     }
 });
+// project.variables.value.vv = new Variable({name: "vv", type: Template.typeFromBracket("(")}); // 프로젝트에 글로벌 변수를 추가. 안하면 로컬에서만 유효.
 
 get.setParam("name", Literal.from("vv"))
 log.setParam("val", get)
 project.thingGroup.children[0].blockGroups[0] =
 project.thingGroup.children[0].blockGroups[0].attach(BlockGroup.fromBlock(log));
 
-console.log(project.thingGroup.children[0].blockGroups[0].blocks[1])
 project.run();
+setTimeout(async () => {
+    console.log("global", project.variables.value.vv?.value);
+}, 1000);
