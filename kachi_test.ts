7387d3ce24bb9e002b834567ba9dd50dc1c14983
Gnlow <entrydark@gmail.com>
2020-03-26 20:57:58 +0900

    이벤트 id랑 name을 바꿔끼움

--- kachi_test.ts
+++ kachi_test.ts
@@ -1,7 +1,10 @@
 import toDalk from "./index";
 
 import test from "./kachi_test_project";
+import { Local } from "dalkak";
 
 const project = toDalk(test as any);
+    //console.log(project.thingGroup.children[0].blockGroups[0].blocks[1].paramTypes.value.ev.fromString("msg", project, new Local));
+    //console.log(project.events.value)
 
 project.run();
